b3575da77d5c313306a100752b369d603681c85f
emilycentko <emily.centko@gmail.com>
2021-01-19 15:52:15 -0600

    Fixed typo in class name

--- scripts/france/cities/City.js
+++ scripts/france/cities/City.js
@@ -1,7 +1,7 @@
 export const City = (city) => {
     return `
           <div class="cities__card">
-            <img class="citites__card-image" src=${city.image}>
+            <img class="cities__card-image" src=${city.image}>
             <p class="cities__card-name>${city.name}</p>
             <p class="cities__card-text">${city.text}</p>
           </div>
