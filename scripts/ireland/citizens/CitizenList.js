b2afd11734afa4d4158955b164b1d473b1af2c00
Dan Ross <daniellross@gmail.com>
2021-01-19 15:43:05 -0600

    added export function to landmark js

--- scripts/ireland/citizens/CitizenList.js
+++
@@ -1,9 +0,0 @@
-export const citizen = (citizenObj) => {
-    return `
-    <div class="citizens__Card">
-    <img class ="citizens__card-image" src="/images/${citizenObj.image}" alt="">
-    <p class="citizens__card-text">${citizenObj.name}</p>
-    <p class="citizens__card-text">${citizenObj.text}</p>
-</div>
-    `
-}
