7210796f291e0a2bc0fb67e560cbf4c2599e882f
Dan Ross <daniellross@gmail.com>
2021-01-19 13:38:31 -0600

    ireland city data provider is working

---
+++ scripts/ireland/citizens/CitizenDataProvider.js
@@ -0,0 +1,17 @@
+const citizenList = [
+    {
+    name: "",
+    image: "",
+    text: "",
+    },
+    {
+    name: "",
+    image: "",
+    text: "",
+    },
+    {
+    name: "",
+    image: "",
+    text: "",
+    }
+]
