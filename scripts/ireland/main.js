0bc4bf9dbca856247834ce36086a0206f42e678d
Dan Ross <daniellross@gmail.com>
2021-01-20 13:27:33 -0600

    added correct import funcions to main

--- scripts/ireland/main.js
+++ scripts/ireland/main.js
@@ -1,2 +1,8 @@
+import { useCitizen } from "./citizens/CitizenDataProvider.js"
+import { cityList   from "./cities/CityList.js"
 
+import { citizenPrintList } from "./citizens/CitizenList.js"
 
+cityList()
+
+citizenPrintList()
